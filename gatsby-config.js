3dc811e4bf5564f83f56c63e1775db7c49aa5fba
sandulat <alexanderstratulat97@gmail.com>
2020-02-27 22:30:07 +0200

    Added Google Analytics tracking id.

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-131895328-1`,
       },
     },
     `gatsby-plugin-feed`,
