e5768dd6ebbf73a830d17d44fa321cbbc1b91943
sandulat <alexanderstratulat97@gmail.com>
2020-02-28 09:39:43 +0200

    Fixed header gradient on Safari & decreased font size fore code blocks on mobile devices.

--- src/components/top-gradient.js
+++ src/components/top-gradient.js
@@ -4,7 +4,8 @@ const TopGradient = props => (
   <div
     {...props}
     style={{
-      background: `linear-gradient(180deg, rgba(60, 54, 107, 0.6) 0%, rgba(60, 54, 107, 0.2) 45%, #1d1d26 100%)`,
+      background: 'rgba(29, 29, 38, 1)',
+      background: `linear-gradient(180deg, rgba(60, 54, 107, 0.6) 0%, rgba(29, 29, 38, 0.4) 65%, rgba(29, 29, 38, 1) 100%)`,
     }}
   />
 );
