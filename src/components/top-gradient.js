ce36e23bdd9295ba21f376ddbf6336054f49dc57
sandulat <alexanderstratulat97@gmail.com>
2020-02-28 09:52:01 +0200

    Decreased markdown inline code font size on mobile.

--- src/components/top-gradient.js
+++ src/components/top-gradient.js
@@ -4,7 +4,6 @@ const TopGradient = props => (
   <div
     {...props}
     style={{
-      background: 'rgba(29, 29, 38, 1)',
       background: `linear-gradient(180deg, rgba(60, 54, 107, 0.6) 0%, rgba(29, 29, 38, 0.4) 65%, rgba(29, 29, 38, 1) 100%)`,
     }}
   />
