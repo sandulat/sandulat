c58783b6923f8409b389591567b5bdeaf7431c56
sandulat <alexanderstratulat97@gmail.com>
2020-03-06 00:54:23 +0200

    Fixed light mode gradient on Safari.

--- src/components/top-gradient.js
+++ src/components/top-gradient.js
@@ -6,7 +6,7 @@ const TopGradient = ({ dark, ...props }) => (
     style={{
       background: dark
         ? `linear-gradient(180deg, rgba(60, 54, 107, 0.6) 0%, rgba(29, 29, 38, 0.4) 65%, rgba(29, 29, 38, 1) 100%)`
-        : `linear-gradient(180deg, rgba(213, 63, 140, 0.15) 0%, rgba(237, 236, 241, 1) 100%)`,
+        : `linear-gradient(180deg, rgba(213, 63, 140, 0.3) 0%, rgba(237, 236, 241, 0.45) 65%, rgba(237, 236, 241, 1) 100%)`,
     }}
   />
 );
