bf49eb2a614f4525c217545cf00296e067561848
sandulat <alexanderstratulat97@gmail.com>
2020-03-15 12:55:40 +0200

    Move article images styling from css to gatsby config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,9 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 768,
+              backgroundColor: 'transparent',
+              wrapperStyle:
+                'white-space: normal; box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05); border-radius: 0.25rem; overflow:hidden;',
             },
           },
           {
